cfdeda15dbfe1faef2d9d4f24a4f17f4df97aee2
Michael König <m.koenig@go2k.de>
2017-09-14 12:27:10 +0200

    Fehler behoben 2

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -32,6 +32,7 @@ public class Fenster extends JFrame{
 
         this.add(jPanel);
 
+
     }
 
 }
--- src/com/sabel/Testklasse.java
+++ src/com/sabel/Testklasse.java
@@ -4,6 +4,7 @@ public class Testklasse {
 
     public static void main(String[] args) {
         new Fenster();
+
     }
 
 }
