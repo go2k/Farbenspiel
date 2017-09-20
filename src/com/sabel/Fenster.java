64eb8e374d079eba87b2753c9ca530584f5254d9
Michael Koenig <m.koenig@go2k.de>
2017-09-20 21:50:08 +0200

    Button vlick events hinzugefügt

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -1,33 +1,59 @@
 package com.sabel;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Fenster extends JFrame{
+public class Fenster extends JFrame {
 
     private JButton jbtnRot;
     private JButton jbtnGelb;
     private JButton jbtnBlau;
     private JPanel jPanel;
 
-    public  Fenster(){
+    public Fenster() {
         this.setTitle("Farbenauswahl");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         initComponents();
-        this.setSize(300,300);
+        this.setSize(300, 300);
         this.setVisible(true);
 
     }
 
-    private void initComponents(){
+    private void initComponents() {
         jPanel = new JPanel();
+        jPanel.setBackground(Color.pink);
         jbtnBlau = new JButton();
         jbtnBlau.setText("Blau");
+        jbtnBlau.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.BLUE);
+            }
+        });
+
         jPanel.add(jbtnBlau);
 
         jbtnGelb = new JButton("Gelb");
+        jbtnGelb.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.YELLOW);
+            }
+        });
         jPanel.add(jbtnGelb);
 
         jbtnRot = new JButton("Rot");
+        jbtnRot.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.RED);
+            }
+        });
         jPanel.add(jbtnRot);
 
         this.add(jPanel);
