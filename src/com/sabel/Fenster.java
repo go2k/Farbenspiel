04e8a88650b0f678228308a4f9b673fdc6b72763
Michael König <m.koenig@go2k.de>
2017-09-21 09:00:25 +0200

    FensterAL imolementiert

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -1,6 +1,9 @@
 package com.sabel;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Fenster extends JFrame{
 
@@ -18,21 +21,40 @@ public class Fenster extends JFrame{
 
     }
 
+
     private void initComponents(){
         jPanel = new JPanel();
         jbtnBlau = new JButton();
         jbtnBlau.setText("Blau");
+        jbtnBlau.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.blue);
+            }
+        });
+
         jPanel.add(jbtnBlau);
 
         jbtnGelb = new JButton("Gelb");
+        jbtnGelb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.yellow);
+            }
+        });
         jPanel.add(jbtnGelb);
 
         jbtnRot = new JButton("Rot");
+        jbtnRot.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.red);
+            }
+        });
         jPanel.add(jbtnRot);
 
         this.add(jPanel);
-
-
     }
+
 
 }
