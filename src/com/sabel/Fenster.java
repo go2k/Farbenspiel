c7683fb6f26e74a7cd87f6f9fd50c72f81794dd6
Michael König <m.koenig@go2k.de>
2017-09-21 09:06:17 +0200

    Project gecleared

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -5,56 +5,33 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Fenster extends JFrame{
+public class Fenster extends JFrame {
 
     private JButton jbtnRot;
     private JButton jbtnGelb;
     private JButton jbtnBlau;
     private JPanel jPanel;
 
-    public  Fenster(){
+    public Fenster() {
         this.setTitle("Farbenauswahl");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         initComponents();
-        this.setSize(300,300);
+        this.setSize(300, 300);
         this.setVisible(true);
-
     }
 
-
-    private void initComponents(){
+    private void initComponents() {
         jPanel = new JPanel();
         jbtnBlau = new JButton();
         jbtnBlau.setText("Blau");
-        jbtnBlau.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jPanel.setBackground(Color.blue);
-            }
-        });
-
         jPanel.add(jbtnBlau);
-
         jbtnGelb = new JButton("Gelb");
-        jbtnGelb.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jPanel.setBackground(Color.yellow);
-            }
-        });
         jPanel.add(jbtnGelb);
-
         jbtnRot = new JButton("Rot");
-        jbtnRot.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jPanel.setBackground(Color.red);
-            }
-        });
         jPanel.add(jbtnRot);
-
         this.add(jPanel);
     }
 
 
 }
+
---
+++ src/com/sabel/FensterMeins.java
@@ -0,0 +1,60 @@
+package com.sabel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FensterMeins extends JFrame{
+
+    private JButton jbtnRot;
+    private JButton jbtnGelb;
+    private JButton jbtnBlau;
+    private JPanel jPanel;
+
+    public FensterMeins(){
+        this.setTitle("Farbenauswahl");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        initComponents();
+        this.setSize(300,300);
+        this.setVisible(true);
+
+    }
+
+
+    private void initComponents(){
+        jPanel = new JPanel();
+        jbtnBlau = new JButton();
+        jbtnBlau.setText("Blau");
+        jbtnBlau.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.blue);
+            }
+        });
+
+        jPanel.add(jbtnBlau);
+
+        jbtnGelb = new JButton("Gelb");
+        jbtnGelb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.yellow);
+            }
+        });
+        jPanel.add(jbtnGelb);
+
+        jbtnRot = new JButton("Rot");
+        jbtnRot.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.red);
+            }
+        });
+        jPanel.add(jbtnRot);
+
+        this.add(jPanel);
+    }
+
+
+}
