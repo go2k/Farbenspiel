fed6b79a0a5970bfbf7ae6b99e8ed298d568be05
Michael König <m.koenig@go2k.de>
2017-09-21 10:15:50 +0200

    Anonyme Innere Klassen an Buttons implementiert und getetstet

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -16,14 +16,35 @@ public class Fenster extends JFrame {
         this.setTitle("Farbenauswahl");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         initComponents();
+        initEvents();
         this.setSize(300, 300);
         this.setVisible(true);
     }
 
+    private void initEvents() {
+        jbtnRot.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.RED);
+            }
+        });
+        jbtnBlau.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.BLUE);
+            }
+        });
+        jbtnGelb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.YELLOW);
+            }
+        });
+    }
+
     private void initComponents() {
         jPanel = new JPanel();
-        jbtnBlau = new JButton();
-        jbtnBlau.setText("Blau");
+        jbtnBlau = new JButton("Blau");
         jPanel.add(jbtnBlau);
         jbtnGelb = new JButton("Gelb");
         jPanel.add(jbtnGelb);
@@ -31,7 +52,6 @@ public class Fenster extends JFrame {
         jPanel.add(jbtnRot);
         this.add(jPanel);
     }
-
 
 }
 
