18c7f41743211cdfd628ea1b35ef8cf85689e1df
Michael König <m.koenig@go2k.de>
2017-09-21 10:20:27 +0200

    EventFensterKlasse bereinigt

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -5,56 +5,57 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Fenster extends JFrame{
+public class Fenster extends JFrame implements ActionListener{
+
+    // Fensterklasse selbst wird zum  ActionListener
 
     private JButton jbtnRot;
     private JButton jbtnGelb;
     private JButton jbtnBlau;
     private JPanel jPanel;
 
-    public  Fenster(){
+    public Fenster() {
         this.setTitle("Farbenauswahl");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         initComponents();
-        this.setSize(300,300);
+        initEvents();
+        this.setSize(300, 300);
         this.setVisible(true);
+    }
 
+    private void initEvents() {
+        jbtnBlau.addActionListener( this);
+        jbtnGelb.addActionListener(this);
+        jbtnRot.addActionListener(this);
     }
 
 
-    private void initComponents(){
+    private void initComponents() {
         jPanel = new JPanel();
         jbtnBlau = new JButton();
         jbtnBlau.setText("Blau");
-        jbtnBlau.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jPanel.setBackground(Color.blue);
-            }
-        });
-
         jPanel.add(jbtnBlau);
-
         jbtnGelb = new JButton("Gelb");
-        jbtnGelb.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jPanel.setBackground(Color.yellow);
-            }
-        });
         jPanel.add(jbtnGelb);
-
         jbtnRot = new JButton("Rot");
-        jbtnRot.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jPanel.setBackground(Color.red);
-            }
-        });
         jPanel.add(jbtnRot);
-
         this.add(jPanel);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "Rot":
+                jPanel.setBackground(Color.RED);
+                break;
+            case "Blau":
+                jPanel.setBackground(Color.BLUE);
+                break;
+            case "Gelb":
+                jPanel.setBackground(Color.YELLOW);
+                break;
+        }
+    }
 
 }
+
